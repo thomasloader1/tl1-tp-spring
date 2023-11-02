fc2cae8d9ab1d00d9e468d9a909f19053fd3ea5e
TomasAyerbe <tomayerbe@gmail.com>
2023-11-02 20:55:55 -0300

    En el registro de un propietario ahora cuando carga la dirección se agrega un campo de ciudad, provincia, código postal y país para obtener las coordenadas correctamente. Ahora la API Geoapify está en el Controlador y la API Key en el archivo .env

--- src/main/java/com/tallerwebi/presentacion/controladores/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/controladores/ControladorLogin.java
@@ -1,6 +1,10 @@
 package com.tallerwebi.presentacion.controladores;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tallerwebi.dominio.entidad.Bicicleta;
+import com.tallerwebi.dominio.entidad.Coordenada;
 import com.tallerwebi.dominio.entidad.Usuario;
 import com.tallerwebi.dominio.excepcion.UsuarioExistente;
 import com.tallerwebi.dominio.excepcion.UsuarioSinRol;
@@ -8,17 +12,21 @@ import com.tallerwebi.dominio.servicios.ServicioBicicleta;
 import com.tallerwebi.dominio.servicios.ServicioLogin;
 import com.tallerwebi.presentacion.dto.DatosLogin;
 import com.tallerwebi.presentacion.dto.DatosUsuario;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+
 
 @Controller
 public class ControladorLogin {
@@ -29,6 +37,34 @@ public class ControladorLogin {
     public ControladorLogin(ServicioLogin servicioLogin, ServicioBicicleta servicioBicicleta) {
         this.servicioLogin = servicioLogin;
         this.servicioBicicleta = servicioBicicleta;
+    }
+
+    @NotNull
+    private static String getApiUrl(DatosUsuario datosUsuario) {
+        Dotenv dotenv = Dotenv.configure().load();
+        String apiKey = dotenv.get("GEOAPIFY_API_KEY");
+        String apiUrl = "https://api.geoapify.com/v1/geocode/search?apiKey=" + apiKey;
+
+        if (!datosUsuario.getDireccion().isEmpty()) {
+            apiUrl += "&text=" + datosUsuario.getDireccion();
+        }
+
+        if (!datosUsuario.getCiudad().isEmpty()) {
+            apiUrl += "&city=" + datosUsuario.getCiudad();
+        }
+
+        if (!datosUsuario.getProvincia().isEmpty()) {
+            apiUrl += "&state=" + datosUsuario.getProvincia();
+        }
+
+        if (!datosUsuario.getCodigoPostal().isEmpty()) {
+            apiUrl += "&postcode=" + datosUsuario.getCodigoPostal();
+        }
+
+        if (!datosUsuario.getPais().isEmpty()) {
+            apiUrl += "&country=" + datosUsuario.getPais();
+        }
+        return apiUrl;
     }
 
     @RequestMapping("/login")
@@ -62,6 +98,9 @@ public class ControladorLogin {
     public ModelAndView registrarme(@ModelAttribute("datosUsuario") DatosUsuario datosUsuario) {
         ModelMap model = new ModelMap();
         try {
+            Coordenada coordenada = obtenerCoordenadas(datosUsuario);
+            datosUsuario.setLatitud(coordenada.getLatitude());
+            datosUsuario.setLongitud(coordenada.getLongitude());
             servicioLogin.registrar(datosUsuario);
         } catch (UsuarioExistente e) {
             model.put("error", "El usuario ya existe");
@@ -96,7 +135,7 @@ public class ControladorLogin {
         if (usuario == null) {
             return new ModelAndView("redirect:/login");
         }
-        
+
         model.put("bicicletas" , bicicletas);
         model.put("usuario", usuario);
         return new ModelAndView("home", model);
@@ -115,5 +154,32 @@ public class ControladorLogin {
     public ModelAndView logout(HttpSession session) {
         session.invalidate();
         return new ModelAndView("redirect:/login");
+    }
+
+    private Coordenada obtenerCoordenadas(DatosUsuario datosUsuario) throws JsonProcessingException {
+        String apiUrl = getApiUrl(datosUsuario);
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(apiUrl, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response);
+        JsonNode features = jsonNode.get("features");
+        JsonNode firstFeature = features.get(0);
+        JsonNode geometry = firstFeature.get("geometry");
+
+        if (geometry != null && geometry.has("coordinates")) {
+            JsonNode coordinates = geometry.get("coordinates");
+            if (coordinates.isArray() && coordinates.size() == 2) {
+                Double latitude = coordinates.get(1).asDouble();
+                Double longitude = coordinates.get(0).asDouble();
+
+                Coordenada coordenada = new Coordenada();
+                coordenada.setLatitude(latitude);
+                coordenada.setLongitude(longitude);
+
+                return coordenada;
+            }
+        }
+        return null;
     }
 }
