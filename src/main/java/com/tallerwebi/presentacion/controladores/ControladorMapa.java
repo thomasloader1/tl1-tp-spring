3dc13f21267e6131e238b5609a4c918eb66aa1ab
TomasAyerbe <tomayerbe@gmail.com>
2023-11-08 20:25:30 -0300

    Agrego mapa de la API de Google Maps. El usuario puede visualizar la distancia (caminando, en bicicleta o auto) en tiempo hacia las bicicleterías más cercanas.

--- src/main/java/com/tallerwebi/presentacion/controladores/ControladorMapa.java
+++ src/main/java/com/tallerwebi/presentacion/controladores/ControladorMapa.java
@@ -1,5 +1,8 @@
 package com.tallerwebi.presentacion.controladores;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tallerwebi.dominio.entidad.Coordenada;
 import com.tallerwebi.dominio.entidad.Usuario;
 import com.tallerwebi.dominio.servicios.DistanciaComparador;
@@ -59,19 +62,30 @@ public class ControladorMapa {
         if (usuario != null) {
             ModelMap modelo = new ModelMap();
             Dotenv dotenv = Dotenv.configure().load();
-            String apiKey = dotenv.get("IPDATA_API_KEY");
-            String apiUrl = "https://api.ipdata.co?fields=latitude,longitude&api-key=" + apiKey;
+            String ipDataApiKey = dotenv.get("IPDATA_API_KEY");
+            String googleMapsApiKey = dotenv.get("GOOGLE_MAPS_API_KEY");
+            String ipDataApiUrl = "https://api.ipdata.co?fields=latitude,longitude&api-key=" + ipDataApiKey;
+
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<Coordenada> response = restTemplate.exchange(apiUrl, HttpMethod.GET, null, Coordenada.class);
+            ResponseEntity<Coordenada> response = restTemplate.exchange(ipDataApiUrl, HttpMethod.GET, null, Coordenada.class);
             if (response.getStatusCode() == HttpStatus.OK) {
                 Coordenada coordenada = response.getBody();
                 modelo.put("latitudActual", coordenada.getLatitude());
                 modelo.put("longitudActual", coordenada.getLongitude());
+
                 List<Usuario> propietarios = servicioMapa.obtenerPropietarios();
                 DistanciaComparador comparador = new DistanciaComparador(coordenada.getLatitude(), coordenada.getLongitude());
                 propietarios.sort(comparador);
                 modelo.put("propietarios", propietarios);
                 modelo.put("distancias", obtenerDistancias(propietarios, coordenada));
+
+                try {
+                    modelo.put("tiemposDeLlegadaCaminando", obtenerTiempoDeLlegadaCaminando(googleMapsApiKey, coordenada, propietarios));
+                    modelo.put("tiemposDeLlegadaEnBicicleta", obtenerTiempoDeLlegadaEnBicicleta(googleMapsApiKey, coordenada, propietarios));
+                    modelo.put("tiemposDeLlegadaEnAuto", obtenerTiempoDeLlegadaEnAuto(googleMapsApiKey, coordenada, propietarios));
+                } catch (JsonProcessingException e) {
+                    modelo.put("error", "Hubo un error al llamar a la API");
+                }
             } else {
                 modelo.put("error", "Hubo un error al llamar a la API");
             }
@@ -79,6 +93,50 @@ public class ControladorMapa {
             return new ModelAndView("mapa", modelo);
         }
         return new ModelAndView("redirect:/login");
+    }
+
+    private List<String> obtenerTiempoDeLlegadaCaminando(String googleMapsApiKey, Coordenada coordenada, List<Usuario> propietarios) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        List<String> tiempos = new LinkedList<>();
+        for (Usuario propietario : propietarios) {
+            ResponseEntity<String> distanceMatrixResponse = restTemplate.exchange("https://maps.googleapis.com/maps/api/distancematrix/json?key=" + googleMapsApiKey + "&origins=" + coordenada.getLatitude() + "," + coordenada.getLongitude() + "&destinations=" + propietario.getLatitud() + "," + propietario.getLongitud() + "&mode=walking", HttpMethod.GET, null, String.class);
+            agregarTiempoDeLlegada(tiempos, distanceMatrixResponse);
+        }
+
+        return tiempos;
+    }
+
+    private List<String> obtenerTiempoDeLlegadaEnBicicleta(String googleMapsApiKey, Coordenada coordenada, List<Usuario> propietarios) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        List<String> tiempos = new LinkedList<>();
+        for (Usuario propietario : propietarios) {
+            ResponseEntity<String> distanceMatrixResponse = restTemplate.exchange("https://maps.googleapis.com/maps/api/distancematrix/json?key=" + googleMapsApiKey + "&origins=" + coordenada.getLatitude() + "," + coordenada.getLongitude() + "&destinations=" + propietario.getLatitud() + "," + propietario.getLongitud() + "&mode=bicycling", HttpMethod.GET, null, String.class);
+            agregarTiempoDeLlegada(tiempos, distanceMatrixResponse);
+        }
+
+        return tiempos;
+    }
+
+    private List<String> obtenerTiempoDeLlegadaEnAuto(String googleMapsApiKey, Coordenada coordenada, List<Usuario> propietarios) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        List<String> tiempos = new LinkedList<>();
+        for (Usuario propietario : propietarios) {
+            ResponseEntity<String> distanceMatrixResponse = restTemplate.exchange("https://maps.googleapis.com/maps/api/distancematrix/json?key=" + googleMapsApiKey + "&origins=" + coordenada.getLatitude() + "," + coordenada.getLongitude() + "&destinations=" + propietario.getLatitud() + "," + propietario.getLongitud() + "&mode=driving", HttpMethod.GET, null, String.class);
+            agregarTiempoDeLlegada(tiempos, distanceMatrixResponse);
+        }
+
+        return tiempos;
+    }
+
+    private void agregarTiempoDeLlegada(List<String> tiempos, ResponseEntity<String> distanceMatrixResponse) throws JsonProcessingException {
+        if (distanceMatrixResponse.getStatusCode() == HttpStatus.OK) {
+            String response = distanceMatrixResponse.getBody();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonResponse = objectMapper.readTree(response);
+            JsonNode duration = jsonResponse.get("rows").get(0).get("elements").get(0).get("duration");
+            String tiempo = duration.get("text").asText();
+            tiempos.add(tiempo.replaceAll("hours?\\b", "h"));
+        }
     }
 
 }
