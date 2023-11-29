3b7b614df9083975eb45ecac24d9d547adc97e19
TomasAyerbe <tomayerbe@gmail.com>
2023-11-29 17:45:24 -0300

    Comienzo vistas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,27 +26,46 @@ INSERT INTO Usuario(id, email, nombre, password, rol)
 VALUES (null, 'cliente2@mail.com', 'Gustavo', '1234', 'Cliente');
 
 INSERT INTO Bicicleta(id, descripcion, estadoBicicleta, usuario_id, urlImagen, condicion, precioVenta,
-                      precioAlquilerPorHora)
+                      precioAlquilerPorHora, puntaje)
 VALUES (null, 'Mi primer bicicleta', 'DISPONIBLE', 1,
         "https://www.seguroscatalanaoccidente.com/blog/assets/multimedia/2016/04/bicicleta-tandem.jpg",
-        "PERFECTO_ESTADO", 50000.0, 600.0);
+        "PERFECTO_ESTADO", 50000.0, 600.0, 8);
 INSERT INTO Bicicleta(id, descripcion, estadoBicicleta, usuario_id, urlImagen, condicion, precioVenta,
-                      precioAlquilerPorHora)
+                      precioAlquilerPorHora, puntaje)
+VALUES (null, 'Mi segunda bicicleta', 'DISPONIBLE', 1,
+        "https://www.seguroscatalanaoccidente.com/blog/assets/multimedia/2016/04/bicicleta-tandem.jpg",
+        "PERFECTO_ESTADO", 50000.0, 600.0, 5);
+INSERT INTO Bicicleta(id, descripcion, estadoBicicleta, usuario_id, urlImagen, condicion, precioVenta,
+                      precioAlquilerPorHora, puntaje)
+VALUES (null, 'Mi tercer bicicleta', 'DISPONIBLE', 1,
+        "https://www.seguroscatalanaoccidente.com/blog/assets/multimedia/2016/04/bicicleta-tandem.jpg",
+        "PERFECTO_ESTADO", 50000.0, 600.0, 5);
+
+INSERT INTO Bicicleta(id, descripcion, estadoBicicleta, usuario_id, urlImagen, condicion, precioVenta,
+                      precioAlquilerPorHora, puntaje)
 VALUES (null, 'Mi segunda bicicleta', 'REQUIERE_REPARACION', 1,
         "https://www.seguroscatalanaoccidente.com/blog/assets/multimedia/2016/04/bicicleta-cruiser.jpg", "BUENO_ESTADO",
-        50000.0, 600.0);
+        50000.0, 600.0, 4);
 INSERT INTO Bicicleta(id, descripcion, estadoBicicleta, usuario_id, urlImagen, condicion, precioVenta,
-                      precioAlquilerPorHora)
+                      precioAlquilerPorHora, puntaje)
 VALUES (null, 'Bicicleta roja', 'EN_USO', 2,
         "https://www.seguroscatalanaoccidente.com/blog/assets/multimedia/2016/04/bicicleta-fixie.jpg", "MAL_ESTADO",
-        50000.0, 600.0);
+        50000.0, 600.0, 2);
 
 INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
-VALUES (null, 'Podría ser mejor.', current_timestamp, 3, 1, 11);
+VALUES (null, 'Podría ser mejor.', current_timestamp, 3, 1, 3);
 INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
-VALUES (null, 'Excelente.', current_timestamp, 5, 1, 11);
+VALUES (null, 'Muy buena bicicleta.', current_timestamp, 5, 1, 4);
 INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
-VALUES (null, 'No me gustó.', current_timestamp, 1, 1, 11);
+VALUES (null, 'Podría ser mejor.', current_timestamp, 5, 2, 3);
+INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
+VALUES (null, 'Podría ser mejor.', current_timestamp, 2, 3, 3);
+INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
+VALUES (null, 'Podría ser mejor.', current_timestamp, 1, 3, 3);
+INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
+VALUES (null, 'Podría ser mejor.', current_timestamp, 1, 3, 3);
+INSERT INTO Resena(id, comentario, fecha, puntaje, bicicleta_id, usuario_id)
+VALUES (null, 'Podría ser mejor.', current_timestamp, 1, 3, 3);
 
 
 INSERT INTO Alquiler(id, cantidadHoras, estadoAlquiler, bicicleta_id, usuario_id)
